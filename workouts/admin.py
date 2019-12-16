85ca76d022c8ff56173b6906210c7ad89ae17b9c
c0chenji <jarick@live.ca>
2019-12-15 21:38:24 -0500

    created models

--- workouts/admin.py
+++ workouts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import WorkOutInput
 # Register your models here.
+class WorkOutInputModelAdmin(admin.ModelAdmin):
+    list_display=['first_name','excercise','duration']
+
+    class Meta:
+        model = WorkOutInput
+
+admin.site.register(WorkOutInput, WorkOutInputModelAdmin)
--- workouts/models.py
+++ workouts/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=100)
+    gender_type= {('F','Female'),('M','Male'),}
+    gender = models.CharField(max_length=1, choices=gender_type)
+    created_on= models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return '%s %s' % (self.first_name, self.last_name)
+
+class WorkOutInput(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    duration = models.CharField(max_length=30)
+    set_count = models.CharField(max_length=30)
+    break_time = models.CharField(max_length=30)
+    excercise_category = {('PU','PushUp'),('SU','SitUp'),('PL','Plank')}
+    excercise = models.CharField(max_length=1, choices=excercise_category)
+    activity_time = models.DateTimeField(auto_now_add=True)
+    completion = models.BooleanField(default=False)
+    def __str__(self):
+        return '%s %s' % (self.excercise, self.activity_time)
+
+class Biometrics(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    age = models.CharField(max_length=30)
+    height = models.CharField(max_length=30)
+    weight = models.CharField(max_length=30)
+    updated = models.DateTimeField(auto_now=True, auto_now_add= False)
+    def __str__(self):
+        return '%s %s %s ' % (self.age, self.height, self.weight)
